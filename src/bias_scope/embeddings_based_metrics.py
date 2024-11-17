5d614fa702838131ef450fe85db3d5949b521c4c
ChadiHelwe <chadi.helwe@gmail.com>
2024-11-17 03:18:35 +0100

    update

--- src/bias_scope/embeddings_based_metrics.py
+++ src/bias_scope/embeddings_based_metrics.py
@@ -2,5 +2,10 @@ import numpy as np
 import torch
 
 
-def weat():
+def weat(
+    target_embeddings1: torch.Tensor | np.ndarray,
+    target_embeddings2: torch.Tensor | np.ndarray,
+    attribute_embeddings1: torch.Tensor | np.ndarray,
+    attribute_embeddings2: torch.Tensor | np.ndarray,
+) -> float:
     pass
