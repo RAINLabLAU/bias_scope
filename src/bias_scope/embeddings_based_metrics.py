f8023ff83c2184950e0b2eddbb98141e0607a7ca
ChadiHelwe <chadi.helwe@gmail.com>
2024-11-17 03:19:29 +0100

    update

--- src/bias_scope/embeddings_based_metrics.py
+++ src/bias_scope/embeddings_based_metrics.py
@@ -8,4 +8,16 @@ def weat(
     attribute_embeddings1: torch.Tensor | np.ndarray,
     attribute_embeddings2: torch.Tensor | np.ndarray,
 ) -> float:
+    """Computes the Word Embedding Association Test (WEAT) score.
+
+    Args:
+        target_embeddings1 (torch.Tensor | np.ndarray): target embeddings for the first target group
+        target_embeddings2 (torch.Tensor | np.ndarray): target embeddings for the second target group
+        attribute_embeddings1 (torch.Tensor | np.ndarray): attribute embeddings for the first attribute group
+        attribute_embeddings2 (torch.Tensor | np.ndarray): attribute embeddings for the second attribute group
+
+    Returns:
+        float: weat score
+    """
     pass
+
