51ac39325b98b5f2cc90b0dad09dd3a9e09696bd
ChadiHelwe <chadi.helwe@gmail.com>
2026-03-02 17:17:08 +0200

    add folder for examples

--- src/bias_scope/prompts_based/bbq.py
+++ src/bias_scope/prompts_based/bbq.py
@@ -5,5 +5,8 @@ from src.bias_scope.base import PromptBasedMetric
 
 class BBQMetric(PromptBasedMetric):
 
-    def evaluate(self, model_name, api_key=None):
+    def __init__(self, api_key: str = None):
+        self.api_key = api_key
+
+    def evaluate(self, model_name):
         pass
